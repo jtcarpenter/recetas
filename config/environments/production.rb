9248184a09fc09e30edbeda8e50c39ead94d60cc
jason <jtcarpenter@gmail.com>
2012-11-02 19:41:18 +0000

    Changed production config for email.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,6 @@ Recetas::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => 'recetas.martajason.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 end
