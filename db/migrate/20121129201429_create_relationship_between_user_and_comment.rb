fee7fa2f65efe12beae4a317160d51a4b9ed280a
jason <jtcarpenter@gmail.com>
2012-12-01 13:52:34 +0000

    Removed commenter removals from migration.

--- db/migrate/20121129201429_create_relationship_between_user_and_comment.rb
+++ db/migrate/20121129201429_create_relationship_between_user_and_comment.rb
@@ -1,11 +1,11 @@
 class CreateRelationshipBetweenUserAndComment < ActiveRecord::Migration
   def up
-    remove_column :comments, :commenter, :string
+    #remove_column :comments, :commenter, :string
     add_column :comments, :user_id, :integer
   end
 
   def down
-    add_column :comments, :commenter, :string
+    #add_column :comments, :commenter, :string
     remove_column :comments, :user_id, :integer
   end
 end
