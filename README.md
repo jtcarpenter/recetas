a0c6967c576c4bf5083232433b31ecb3b707ae83
jason <jtcarpenter@gmail.com>
2012-12-06 20:40:30 +0000

    Configured CKEditor to force paste as plain text by default, closes #25.

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Install Imagick
 
 $ cp config/application.example.yml config/application.yml
 
-For postgres run rails in staging environment
-$ rails s -e staging
-
 If using bundler, wihout postgres installation run:
 $ bundle install --without postgres
+
+For postgres run rails in staging environment
+$ rails s -e staging
 
 $ add config to heroku
 $ heroku config:add AWS_ACCESS_KEY_ID=xxx AWS_SECRET_ACCESS_KEY=xxx GMAIL_USER_NAME=xxx GMAIL_PASSWORD=xxx
--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -6,11 +6,12 @@ CKEDITOR.editorConfig = function( config )
   config.defaultLanguage = lang;
   config.height = 500;
   config.format_tags = 'p;h1;h2;h3;h4;h5;h6';
+  config.forcePasteAsPlainText = true;
 
   config.toolbar_MyToolbar =
   [
     { name: 'document', items : [ 'Maximize', '-', 'NewPage','Preview' ] },
-    { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','-','Undo','Redo' ] },
+    { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteFromWord','-','Undo','Redo' ] },
     { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt', '-', 'SpecialChar' ] },
     { name: 'styles', items : [ 'Format', 'Bold','Italic','Strike','-','RemoveFormat' ] },
     { name: 'types', items : [ 'NumberedList','BulletedList', '-', 'Link', 'Unlink' ] }
