c4f8fc02a7862345fb623224c09e0aa9f7bfd8e9
jason <jtcarpenter@gmail.com>
2013-02-24 19:28:36 +0000

    Added print.css to precompile config in production.rb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,8 @@ Recetas::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( admin.js )
+  config.assets.precompile += %w( print.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -67,5 +69,4 @@ Recetas::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'recetas.martajason.com' }
   config.assets.precompile += Ckeditor.assets
-  config.assets.precompile += %w( admin.js )
 end
