d7072f52e2a16c99d14fd1c961a8a380615b26eb
jason <jtcarpenter@gmail.com>
2013-02-24 12:39:49 +0000

    Changed production environment, attempted fix for forgotten password issue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Recetas::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'martajason.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.assets.precompile += Ckeditor.assets
   config.assets.precompile += %w( admin.js )
 end
